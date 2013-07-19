03fd6c00882c4951cabc493c3ffeaf690d2d4de5
Anthony Tiradani <tiradani@fnal.gov>
2013-07-19 09:32:06 -0500

    added helper fork function

--- convenient_python/py_utils/__init__.py
+++ convenient_python/py_utils/__init__.py
@@ -1,10 +1,48 @@
-
+import os
+import cPickle
 
 def chunk_list(orig_list, chunk_size):
-    """ 
+    """
     Source: http://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks-in-python
 
     Yield successive n-sized chunks (chunk_size) from orig_list.
     """
     for i in xrange(0, len(orig_list), chunk_size):
         yield orig_list[i:i+chunk_size]
+
+def fork(function, *args, **kwargs):
+    """
+    Convenience function to wrap forking calls.  It will fork and call a 
+    function with the arguments specified.
+
+    Example:
+
+    def add(i, j): return i+j
+        d = fork_in_bg(add, i, j)
+
+    @type function: function_pointer
+    @param function: The function which will be executed after the fork
+
+    @type *args: list
+    @param *args: the argument list to pass to the function
+
+    @type **kwargs: dict
+    @param **kwargs: the named argument dict to pass to the function
+
+    @return: dict with {'r': fd, 'pid': pid} where fd is the stdout from a pipe.
+    """
+
+    r, w = os.pipe()
+    pid = os.fork()
+    if pid == 0:
+        os.close(r)
+        try:
+            out = function(*args, **kwargs)
+            os.write(w, cPickle.dumps(out))
+        finally:
+            os.close(w)
+            os._exit(0)
+    else:
+        os.close(w)
+
+    return {'r': r, 'pid': pid}
